38ba6bf2a5ce1acbc90226a6dc45b1266a40df90
Phil <philipp.geueke.dev@gmail.com>
2025-01-07 14:40:32 +0100

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Philipp Geueke on 07.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
