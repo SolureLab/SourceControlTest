d811b246638e1d3230073bff3dd6ec2ca82e3edf
Phil <philipp.geueke.dev@gmail.com>
2025-01-07 15:44:43 +0100

    [Clean] Add commit messages documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Philipp Geueke on 07.01.25.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE: [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION: [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION: [Bug] Description of the Bug
+ 
+ MUNDANE TASKS: [Clean] Description of changes
+ 
+ RELEASE: [Release] Description of Release
+ 
+ 
+ 
+ 
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
